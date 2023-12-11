a500243e33146ebf1de6caa275137e71421ef265
Carlos Caceres <carlosmicro33@gmail.com>
2023-12-11 18:08:46 -0500

    widget update

--- app/src/main/java/com/example/equiposiete/view/Widget.kt
+++ app/src/main/java/com/example/equiposiete/view/Widget.kt
@@ -1,8 +1,12 @@
 package com.example.equiposiete.view
 
+import android.app.PendingIntent
 import android.appwidget.AppWidgetManager
 import android.appwidget.AppWidgetProvider
+import android.content.ComponentName
 import android.content.Context
+import android.content.Intent
+import android.util.Log
 import android.widget.RemoteViews
 import com.example.equiposiete.R
 
@@ -28,21 +32,58 @@ class Widget : AppWidgetProvider() {
     override fun onDisabled(context: Context) {
         // Enter relevant functionality for when the last widget is disabled
     }
+
+    override fun onReceive(context: Context, intent: Intent) {
+        super.onReceive(context, intent)
+        if ("ACTION_IMAGE_CLICK" == intent.action) {
+            // Si se hace clic en el ImageView, imprime la palabra "clic"
+            Log.d("TuAppWidget", "Clic")
+        } else if ("ACTION_BUTTON_CLICK" == intent.action) {
+            // Si se hace clic en el Button, cambia el texto del TextView
+            val appWidgetManager = AppWidgetManager.getInstance(context)
+            val appWidgetIds = appWidgetManager.getAppWidgetIds(ComponentName(context, Widget::class.java))
+
+            for (appWidgetId in appWidgetIds) {
+                val views = RemoteViews(context.packageName, R.layout.widget)
+                updateTextSaldo(context, views)
+            }
+        }
+    }
+}
+
+private fun updateTextSaldo(context: Context, views: RemoteViews) {
+    views.setTextViewText(R.id.text_saldo, "$ 1,000")
 }
 
 internal fun updateAppWidget(
     context: Context,
     appWidgetManager: AppWidgetManager,
     appWidgetId: Int
-) {
-    /*
-    val widgetText = context.getString(R.string.appwidget_text)
-    // Construct the RemoteViews object
+)
+{
     val views = RemoteViews(context.packageName, R.layout.widget)
-    views.setTextViewText(R.id.appwidget_text, widgetText)
+
+    // Configura un PendingIntent para el clic en el ImageView
+    val intentImageClick = Intent(context, Widget::class.java)
+    intentImageClick.action = "ACTION_IMAGE_CLICK"
+    val pendingIntentImageClick =
+        PendingIntent.getBroadcast(context, 0, intentImageClick, PendingIntent.FLAG_UPDATE_CURRENT)
+
+    // Establece el PendingIntent en el ImageView
+    views.setOnClickPendingIntent(R.id.iconImageView, pendingIntentImageClick)
+
+    // Configura un PendingIntent para el clic en el Button
+    val intentButtonClick = Intent(context, Widget::class.java)
+    intentButtonClick.action = "ACTION_BUTTON_CLICK"
+    val pendingIntentButtonClick =
+        PendingIntent.getBroadcast(context, 0, intentButtonClick, PendingIntent.FLAG_UPDATE_CURRENT)
+
+    // Establece el PendingIntent en el Button
+    views.setOnClickPendingIntent(R.id.ButtonRandom, pendingIntentButtonClick)
+
+    // Llama a la función para actualizar el texto del saldo
+    updateTextSaldo(context, views)
 
     // Instruct the widget manager to update the widget
     appWidgetManager.updateAppWidget(appWidgetId, views)
-
-     */
 }
