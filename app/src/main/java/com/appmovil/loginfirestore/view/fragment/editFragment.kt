7d6b4e63d315de9f8a0cf435f4bfdb97ff8d15ae
Santiago Gutierrez Hincapie <gutierrez.santiago@correounivalle.edu.co>
2023-12-13 16:27:06 -0500

    creaated edit_product branch, created edit_product layout and initiate the kotlin file for the logic

--- app/src/main/java/com/appmovil/loginfirestore/view/fragment/editFragment.kt
+++ app/src/main/java/com/appmovil/loginfirestore/view/fragment/editFragment.kt
@@ -1,4 +1,4 @@
-import android.content.SharedPreferences
+package com.appmovil.loginfirestore.view.fragment;
 import android.graphics.Typeface
 import android.os.Bundle
 import android.text.Editable
@@ -11,18 +11,18 @@ import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.appmovil.loginfirestore.R
 import com.google.firebase.firestore.FirebaseFirestore
-import com.appmovil.loginfirestore.databinding.FragmentAddProductBinding
+import com.appmovil.loginfirestore.databinding.FragmentEditProductBinding
 import com.appmovil.loginfirestore.model.Articulo
 
-class addFragment : Fragment() {
-
-    private lateinit var binding: FragmentAddProductBinding
+class editFragment : Fragment() {
+    private lateinit var binding: FragmentEditProductBinding
     private val db = FirebaseFirestore.getInstance()
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        binding = FragmentAddProductBinding.inflate(inflater)
+        binding = FragmentEditProductBinding.inflate(inflater)
         binding.lifecycleOwner = this
         return binding.root
     }
@@ -31,26 +31,23 @@ class addFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
         goHome()
         setupButton()
-        binding.saveButton.isEnabled = false
+        binding.editButton.isEnabled = false
         // Configura los oyentes y la lógica según sea necesario.
     }
 
-
-
     private fun setupButton() {
         // Configura el botón (Criterio 6 y 7)
-        binding.saveButton.setOnClickListener {
+        binding.editButton.setOnClickListener {
             if (camposEstanLlenos()) {
                 // Lógica para guardar en Firestore y mostrar en la lista de productos (Criterio 8)
-                guardarProducto()
+                editarProducto()
             }
         }
 
         // Observador de cambios en los campos para habilitar/deshabilitar el botón
-        binding.addTextProductCode.addTextChangedListener(textWatcher)
-        binding.addArticleName.addTextChangedListener(textWatcher)
-        binding.addPrice.addTextChangedListener(textWatcher)
-        binding.addQuantity.addTextChangedListener(textWatcher)
+        binding.editArticleName.addTextChangedListener(textWatcher)
+        binding.editPrice.addTextChangedListener(textWatcher)
+        binding.editQuantity.addTextChangedListener(textWatcher)
     }
 
     private val textWatcher = object : TextWatcher {
@@ -60,29 +57,41 @@ class addFragment : Fragment() {
 
         override fun afterTextChanged(s: Editable?) {
             // Verificar si todos los campos están llenos y habilitar/deshabilitar el botón (Criterios 6 y 7)
-            binding.saveButton.isEnabled = camposEstanLlenos()
+            binding.editButton.isEnabled = camposEstanLlenos()
             // Cambiar el color y estilo del texto del botón si está habilitado (Criterio 7)
-            binding.saveButton.setTextColor(if (binding.saveButton.isEnabled) resources.getColor(android.R.color.white) else resources.getColor(android.R.color.black))
-            binding.saveButton.setTypeface(null, if (binding.saveButton.isEnabled) Typeface.BOLD else Typeface.NORMAL)
+            binding.editButton.setTextColor(if (binding.editButton.isEnabled) resources.getColor(android.R.color.white) else resources.getColor(android.R.color.black))
+            binding.editButton.setTypeface(null, if (binding.editButton.isEnabled) Typeface.BOLD else Typeface.NORMAL)
         }
     }
 
     private fun camposEstanLlenos(): Boolean {
-        val codigo = binding.addTextProductCode.text.toString()
-        val nombre = binding.addArticleName.text.toString()
-        val precio = binding.addPrice.text.toString()
-        val cantidad = binding.addQuantity.text.toString()
-
-        return codigo.isNotEmpty() && nombre.isNotEmpty() && precio.isNotEmpty() && cantidad.isNotEmpty()
+        val nombre = binding.editArticleName.text.toString()
+        val precio = binding.editPrice.text.toString()
+        val cantidad = binding.editQuantity.text.toString()
+        return nombre.isNotEmpty() && precio.isNotEmpty() && cantidad.isNotEmpty()
     }
 
-    private fun guardarProducto() {
-        val codigo = binding.addTextProductCode.text.toString()
-        val nombre = binding.addArticleName.text.toString()
-        val precio = binding.addPrice.text.toString()
-        val cantidad = binding.addQuantity.text.toString()
+    //////////////////////Edit//////////////////
+    private fun goHome(){
+        binding.backArrowEdit.setOnClickListener{
+            findNavController().navigate(R.id.action_addFragment_to_homeFragment)
+        }
+    }
 
-        if (codigo.isNotEmpty() && nombre.isNotEmpty() && precio.isNotEmpty() && cantidad.isNotEmpty()) {
+    private fun limpiarCampos() {
+        binding.editArticleName.setText("")
+        binding.editPrice.setText("")
+        binding.editQuantity.setText("")
+    }
+
+    ///////////////////////////////edit////////////////////////////
+    private fun editarProducto() {
+        val codigo = binding.textProductCode.text.toString()
+        val nombre = binding.editArticleName.text.toString()
+        val precio = binding.editPrice.text.toString()
+        val cantidad = binding.editQuantity.text.toString()
+
+        if (nombre.isNotEmpty() && precio.isNotEmpty() && cantidad.isNotEmpty()) {
             val articulo = Articulo(codigo.toInt(), nombre, precio.toInt(), cantidad.toInt())
 
             db.collection("articulo").document(articulo.codigo.toString()).set(
@@ -101,16 +110,4 @@ class addFragment : Fragment() {
         }
     }
 
-    private fun goHome(){
-        binding.backArrowAdd.setOnClickListener{
-            findNavController().navigate(R.id.action_addFragment_to_homeFragment)
-        }
-    }
-
-    private fun limpiarCampos() {
-        binding.addArticleName.setText("")
-        binding.addTextProductCode.setText("")
-        binding.addPrice.setText("")
-        binding.addQuantity.setText("")
-    }
 }
